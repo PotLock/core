ec02294253b22c2d4c50a75331df23ada9eb04db
Lachlan Glen <54282009+lachlanglen@users.noreply.github.com>
2023-11-02 14:16:50 -0400

    add source metadata

--- contracts/donation/src/events.rs
+++ contracts/donation/src/events.rs
@@ -20,3 +20,24 @@ pub(crate) fn log_donation_event(donation: &Donation) {
         .as_ref(),
     );
 }
+
+/// source metadata update
+pub(crate) fn log_set_source_metadata_event(source_metadata: &ContractSourceMetadata) {
+    env::log_str(
+        format!(
+            "{}{}",
+            EVENT_JSON_PREFIX,
+            json!({
+                "standard": "potlock",
+                "version": "1.0.0",
+                "event": "set_source_metadata",
+                "data": [
+                    {
+                        "source_metadata": source_metadata,
+                    }
+                ]
+            })
+        )
+        .as_ref(),
+    );
+}
--- contracts/donation/src/lib.rs
+++ contracts/donation/src/lib.rs
@@ -1,9 +1,10 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{LookupMap, UnorderedMap, UnorderedSet};
+use near_sdk::collections::{LazyOption, LookupMap, UnorderedMap, UnorderedSet};
 use near_sdk::json_types::{U128, U64};
 use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{
-    env, log, near_bindgen, require, serde_json::json, AccountId, Balance, BorshStorageKey, Promise,
+    env, log, near_bindgen, require, serde_json::json, AccountId, Balance, BorshStorageKey,
+    PanicOnDefault, Promise,
 };
 
 pub mod constants;
@@ -11,12 +12,14 @@ pub mod donations;
 pub mod events;
 pub mod internal;
 pub mod owner;
+pub mod source;
 pub mod utils;
 pub use crate::constants::*;
 pub use crate::donations::*;
 pub use crate::events::*;
 pub use crate::internal::*;
 pub use crate::owner::*;
+pub use crate::source::*;
 pub use crate::utils::*;
 
 type DonationId = u64;
@@ -29,6 +32,8 @@ pub const EVENT_JSON_PREFIX: &str = "EVENT_JSON:";
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
+    /// Contract "source" metadata, as specified in NEP 0330 (https://github.com/near/NEPs/blob/master/neps/nep-0330.md), with addition of `commit_hash`
+    contract_source_metadata: LazyOption<VersionedContractSourceMetadata>,
     owner: AccountId,
     protocol_fee_basis_points: u32,
     referral_fee_basis_points: u32,
@@ -62,12 +67,14 @@ pub enum StorageKey {
     DonationIdsByDonorIdInner { donor_id: AccountId },
     DonationIdsByFtId,
     DonationIdsByFtIdInner { ft_id: AccountId },
+    SourceMetadata,
 }
 
 #[near_bindgen]
 impl Contract {
     #[init]
     pub fn new(
+        source_metadata: ContractSourceMetadata,
         owner: AccountId,
         protocol_fee_basis_points: u32,
         referral_fee_basis_points: u32,
@@ -75,6 +82,10 @@ impl Contract {
     ) -> Self {
         assert!(!env::state_exists(), "Already initialized");
         Self {
+            contract_source_metadata: LazyOption::new(
+                StorageKey::SourceMetadata,
+                Some(&VersionedContractSourceMetadata::Current(source_metadata)),
+            ),
             owner,
             protocol_fee_basis_points,
             referral_fee_basis_points,
@@ -90,6 +101,16 @@ impl Contract {
 impl Default for Contract {
     fn default() -> Self {
         Self {
+            contract_source_metadata: LazyOption::new(
+                StorageKey::SourceMetadata,
+                Some(&VersionedContractSourceMetadata::Current(
+                    ContractSourceMetadata {
+                        version: "1.0.0".to_string(),
+                        commit_hash: "12345".to_string(),
+                        link: "www.example.com".to_string(),
+                    },
+                )),
+            ),
             owner: AccountId::new_unchecked("".to_string()),
             protocol_fee_basis_points: 0,
             referral_fee_basis_points: 0,
