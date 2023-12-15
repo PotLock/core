b140666f80a940e5f98398dba7cb32177898289d
Lachlan Glen <54282009+lachlanglen@users.noreply.github.com>
2023-12-15 11:52:07 -0500

    add contract source metadata to Registry contract

--- contracts/registry/src/lib.rs
+++ contracts/registry/src/lib.rs
@@ -1,17 +1,26 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{LookupMap, UnorderedSet};
+use near_sdk::collections::{LazyOption, LookupMap, UnorderedSet};
 use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::{env, log, near_bindgen, require, AccountId, Balance, BorshStorageKey, Promise};
+use near_sdk::{
+    env, log, near_bindgen, require, serde_json::json, AccountId, Balance, BorshStorageKey,
+    PanicOnDefault, Promise,
+};
 
 pub mod admins;
+pub mod constants;
+pub mod events;
 pub mod internal;
 pub mod owner;
 pub mod projects;
+pub mod source;
 pub mod utils;
 pub use crate::admins::*;
+pub use crate::constants::*;
+pub use crate::events::*;
 pub use crate::internal::*;
 pub use crate::owner::*;
 pub use crate::projects::*;
+pub use crate::source::*;
 pub use crate::utils::*;
 
 type ProjectId = AccountId;
@@ -21,10 +30,15 @@ type TimestampMs = u64;
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
+    /// Contract superuser
     owner: AccountId,
+    /// Contract admins (can be added/removed by owner)
     admins: UnorderedSet<AccountId>,
-    project_ids: UnorderedSet<ProjectId>,
+    /// Records of all Projects deployed by this Registry, indexed at their account ID, versioned for easy upgradeability
+    project_ids: UnorderedSet<ProjectId>, // NB: this is unnecessary, but retained for now as it is implemented in v0
     projects_by_id: LookupMap<ProjectId, VersionedProjectInternal>,
+    /// Contract "source" metadata, as specified in NEP 0330 (https://github.com/near/NEPs/blob/master/neps/nep-0330.md), with addition of `commit_hash`
+    contract_source_metadata: LazyOption<VersionedContractSourceMetadata>,
 }
 
 #[derive(BorshSerialize, BorshDeserialize)]
@@ -46,18 +60,27 @@ pub enum StorageKey {
     Admins,
     ProjectIds,
     ProjectsById,
+    SourceMetadata,
 }
 
 #[near_bindgen]
 impl Contract {
     #[init]
-    pub fn new(owner: AccountId, admins: Vec<AccountId>) -> Self {
+    pub fn new(
+        owner: AccountId,
+        admins: Vec<AccountId>,
+        source_metadata: ContractSourceMetadata,
+    ) -> Self {
         assert!(!env::state_exists(), "Already initialized");
         Self {
             owner,
             admins: account_vec_to_set(admins, StorageKey::Admins),
             project_ids: UnorderedSet::new(StorageKey::ProjectIds),
             projects_by_id: LookupMap::new(StorageKey::ProjectsById),
+            contract_source_metadata: LazyOption::new(
+                StorageKey::SourceMetadata,
+                Some(&VersionedContractSourceMetadata::Current(source_metadata)),
+            ),
         }
     }
 }
@@ -69,6 +92,7 @@ impl Default for Contract {
             admins: UnorderedSet::new(StorageKey::Admins),
             project_ids: UnorderedSet::new(StorageKey::ProjectIds),
             projects_by_id: LookupMap::new(StorageKey::ProjectsById),
+            contract_source_metadata: LazyOption::new(StorageKey::SourceMetadata, None),
         }
     }
 }
