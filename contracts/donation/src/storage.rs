65e35894639cad49840bdb84d552584095ac53b9
Lachlan Glen <54282009+lachlanglen@users.noreply.github.com>
2024-01-25 11:26:20 -0500

    rename storage_add to storage_deposit

--- contracts/donation/src/storage.rs
+++ contracts/donation/src/storage.rs
@@ -3,32 +3,31 @@ use crate::*;
 #[near_bindgen]
 impl Contract {
     #[payable]
-    pub fn storage_add(&mut self) {
+    pub fn storage_deposit(&mut self) -> U128 {
         let mut deposit = env::attached_deposit();
         let initial_storage_usage = env::storage_usage();
-        if deposit > 0 {
-            let existing_mapping = self.storage_deposits.get(&env::predecessor_account_id());
-            if existing_mapping.is_none() {
-                // insert record here and check how much storage was used, then subtract that cost from the deposit
-                self.storage_deposits
-                    .insert(&env::predecessor_account_id(), &0);
-                let storage_usage = env::storage_usage() - initial_storage_usage;
-                let required_deposit = storage_usage as u128 * env::storage_byte_cost();
-                assert!(
-                    deposit >= required_deposit,
-                    "The deposit is less than the required storage amount."
-                );
-                deposit -= required_deposit;
-            }
-            let account_id = env::predecessor_account_id();
-            let storage_balance = self.storage_balance_of(&account_id);
-            let new_storage_balance = storage_balance.0 + deposit;
+        let existing_mapping = self.storage_deposits.get(&env::predecessor_account_id());
+        if existing_mapping.is_none() {
+            // insert record here and check how much storage was used, then subtract that cost from the deposit
             self.storage_deposits
-                .insert(&account_id, &new_storage_balance);
+                .insert(&env::predecessor_account_id(), &0);
+            let storage_usage = env::storage_usage() - initial_storage_usage;
+            let required_deposit = storage_usage as u128 * env::storage_byte_cost();
+            assert!(
+                deposit >= required_deposit,
+                "The deposit is less than the required storage amount."
+            );
+            deposit -= required_deposit;
         }
+        let account_id = env::predecessor_account_id();
+        let storage_balance = self.storage_balance_of(&account_id);
+        let new_storage_balance = storage_balance.0 + deposit;
+        self.storage_deposits
+            .insert(&account_id, &new_storage_balance);
+        new_storage_balance.into()
     }
 
-    pub fn storage_withdraw(&mut self, amount: Option<U128>) {
+    pub fn storage_withdraw(&mut self, amount: Option<U128>) -> U128 {
         let account_id = env::predecessor_account_id();
         let storage_balance = self.storage_balance_of(&account_id);
         let amount = amount.map(|a| a.0).unwrap_or(storage_balance.0);
@@ -48,6 +47,7 @@ impl Contract {
             let refund = storage_usage as u128 * env::storage_byte_cost();
             Promise::new(account_id).transfer(refund);
         }
+        remainder.into()
     }
 
     pub fn storage_balance_of(&self, account_id: &AccountId) -> U128 {
