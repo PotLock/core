94e9b26bdae21ccd600cce6472069f28a9982bc0
Lachlan Glen <54282009+lachlanglen@users.noreply.github.com>
2024-02-02 10:56:36 -0500

    clean up comments

--- contracts/registry/src/lib.rs
+++ contracts/registry/src/lib.rs
@@ -26,7 +26,7 @@ pub use crate::utils::*;
 type ProjectId = AccountId;
 type TimestampMs = u64;
 
-/// OLD (v1) Registry Contract
+/// OLD (v1) Registry Contract (v1.0.0)
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct ContractV1 {
@@ -37,11 +37,9 @@ pub struct ContractV1 {
     /// Records of all Projects deployed by this Registry, indexed at their account ID, versioned for easy upgradeability
     project_ids: UnorderedSet<ProjectId>, // NB: this is unnecessary, but retained for now as it is implemented in v0
     projects_by_id: LookupMap<ProjectId, VersionedProjectInternal>,
-    // /// Contract "source" metadata, as specified in NEP 0330 (https://github.com/near/NEPs/blob/master/neps/nep-0330.md), with addition of `commit_hash`
-    // contract_source_metadata: LazyOption<VersionedContractSourceMetadata>,
 }
 
-/// Registry Contract
+/// CURRENT Registry Contract (v2.0.0)
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
@@ -153,106 +151,74 @@ impl Contract {
         }
     }
 
-    // for project_id in old_state.project_ids.iter() {
-    //     let project_internal =
-    //         ProjectInternal::from(old_state.projects_by_id.get(&project_id).unwrap());
-    //     // add to projects_by_id
-    //     projects_by_id.insert(
-    //         &project_id,
-    //         &VersionedProjectInternal::Current(project_internal.clone()),
-    //     );
-    //     log!(
-    //         "Migrated project {} with status {:?}",
-    //         project_id,
-    //         project_internal.status
-    //     );
-    //     match project_internal.status {
-    //         ProjectStatus::Pending => {
-    //             pending_project_ids.insert(&project_id);
-    //         }
-    //         ProjectStatus::Approved => {
-    //             approved_project_ids.insert(&project_id);
-    //         }
-    //         ProjectStatus::Rejected => {
-    //             rejected_project_ids.insert(&project_id);
-    //         }
-    //         ProjectStatus::Graylisted => {
-    //             graylisted_project_ids.insert(&project_id);
-    //         }
-    //         ProjectStatus::Blacklisted => {
-    //             blacklisted_project_ids.insert(&project_id);
+    // LEAVING FOR REFERENCE - this is function used to migrate data in upgrade from v1.0.0 to v2.0.0
+    // #[private]
+    // pub fn migrate_chunk_temp(&mut self, project_ids: Vec<ProjectId>) {
+    //     for project_id in project_ids {
+    //         let project = self._deprecated_projects_by_id.get(&project_id);
+    //         if let Some(project) = project {
+    //             let project_internal = ProjectInternal::from(project);
+    //             // add to projects_by_id
+    //             self.projects_by_id.insert(
+    //                 &project_id,
+    //                 &VersionedProjectInternal::Current(project_internal.clone()),
+    //             );
+    //             log!("Migrated project {}", project_id);
+    //             match project_internal.status {
+    //                 ProjectStatus::Pending => {
+    //                     self.pending_project_ids.insert(&project_id);
+    //                 }
+    //                 ProjectStatus::Approved => {
+    //                     self.approved_project_ids.insert(&project_id);
+    //                 }
+    //                 ProjectStatus::Rejected => {
+    //                     self.rejected_project_ids.insert(&project_id);
+    //                 }
+    //                 ProjectStatus::Graylisted => {
+    //                     self.graylisted_project_ids.insert(&project_id);
+    //                 }
+    //                 ProjectStatus::Blacklisted => {
+    //                     self.blacklisted_project_ids.insert(&project_id);
+    //                 }
+    //             }
+    //             self._deprecated_project_ids.remove(&project_id);
+    //             self._deprecated_projects_by_id.remove(&project_id);
+    //         } else {
+    //             log!(format!(
+    //                 "project ID {} not found in deprectaed projects by ID",
+    //                 project_id
+    //             ));
     //         }
     //     }
     // }
 
-    #[private]
-    pub fn migrate_chunk_temp(&mut self, project_ids: Vec<ProjectId>) {
-        for project_id in project_ids {
-            let project = self._deprecated_projects_by_id.get(&project_id);
-            if let Some(project) = project {
-                let project_internal = ProjectInternal::from(project);
-                // if let Some(project_internal) = project_internal.clone() {
-                // add to projects_by_id
-                self.projects_by_id.insert(
-                    &project_id,
-                    &VersionedProjectInternal::Current(project_internal.clone()),
-                );
-                log!("Migrated project {}", project_id);
-                match project_internal.status {
-                    ProjectStatus::Pending => {
-                        self.pending_project_ids.insert(&project_id);
-                    }
-                    ProjectStatus::Approved => {
-                        self.approved_project_ids.insert(&project_id);
-                    }
-                    ProjectStatus::Rejected => {
-                        self.rejected_project_ids.insert(&project_id);
-                    }
-                    ProjectStatus::Graylisted => {
-                        self.graylisted_project_ids.insert(&project_id);
-                    }
-                    ProjectStatus::Blacklisted => {
-                        self.blacklisted_project_ids.insert(&project_id);
-                    }
-                }
-                self._deprecated_project_ids.remove(&project_id);
-                self._deprecated_projects_by_id.remove(&project_id);
-            } else {
-                log!(format!(
-                    "project ID {} not found in deprectaed projects by ID",
-                    project_id
-                ));
-            }
-        }
-    }
-
-    #[private]
-    #[init(ignore_state)]
-    pub fn migrate() -> Self {
-        let old_state: ContractV1 = env::state_read().expect("state read failed");
-        // populate new maps/sets
-        let projects_by_id = UnorderedMap::new(StorageKey::ProjectsById2);
-        let pending_project_ids = UnorderedSet::new(StorageKey::PendingProjectIds);
-        let approved_project_ids = UnorderedSet::new(StorageKey::ApprovedProjectIds);
-        let rejected_project_ids = UnorderedSet::new(StorageKey::RejectedProjectIds);
-        let graylisted_project_ids = UnorderedSet::new(StorageKey::GraylistedProjectIds);
-        let blacklisted_project_ids = UnorderedSet::new(StorageKey::BlacklistedProjectIds);
-        // old_state.project_ids.clear(); // don't need these anymore, but still need to keep mapping at StorageKey::ProjectIds
-        Self {
-            owner: old_state.owner,
-            admins: old_state.admins,
-            _deprecated_project_ids: old_state.project_ids,
-            _deprecated_projects_by_id: old_state.projects_by_id,
-            projects_by_id,
-            pending_project_ids,
-            approved_project_ids,
-            rejected_project_ids,
-            graylisted_project_ids,
-            blacklisted_project_ids,
-            contract_source_metadata: LazyOption::new(StorageKey::SourceMetadata, None),
-            default_project_status: ProjectStatus::Approved,
-        }
-    }
+    // LEAVING FOR REFERENCE - this is the initFunction used in upgrade from v1.0.0 to v2.0.0
+    // #[private]
+    // #[init(ignore_state)]
+    // pub fn migrate() -> Self {
+    //     let old_state: ContractV1 = env::state_read().expect("state read failed");
+    //     // populate new maps/sets
+    //     let projects_by_id = UnorderedMap::new(StorageKey::ProjectsById2);
+    //     let pending_project_ids = UnorderedSet::new(StorageKey::PendingProjectIds);
+    //     let approved_project_ids = UnorderedSet::new(StorageKey::ApprovedProjectIds);
+    //     let rejected_project_ids = UnorderedSet::new(StorageKey::RejectedProjectIds);
+    //     let graylisted_project_ids = UnorderedSet::new(StorageKey::GraylistedProjectIds);
+    //     let blacklisted_project_ids = UnorderedSet::new(StorageKey::BlacklistedProjectIds);
+    //     Self {
+    //         owner: old_state.owner,
+    //         admins: old_state.admins,
+    //         _deprecated_project_ids: old_state.project_ids,
+    //         _deprecated_projects_by_id: old_state.projects_by_id,
+    //         projects_by_id,
+    //         pending_project_ids,
+    //         approved_project_ids,
+    //         rejected_project_ids,
+    //         graylisted_project_ids,
+    //         blacklisted_project_ids,
+    //         contract_source_metadata: LazyOption::new(StorageKey::SourceMetadata, None),
+    //         default_project_status: ProjectStatus::Approved,
+    //     }
+    // }
 }
 
 impl Default for Contract {
